108d578a342fd6f28197c84e86541d1f3b55285b
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-14 10:22:17 -0300

    feat: Creating validation for the login form

--- components/Auth/LoginForm/index.jsx
+++ components/Auth/LoginForm/index.jsx
@@ -5,17 +5,30 @@ import * as Yup from 'yup'
 import { toast } from 'react-toastify'
 
 export default function LoginForm({ showRegisterForm }) {
+  
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData)
+    }
+  })
+
   return (
-    <Form className="login-form">
+    <Form className="login-form" onSubmit={formik.handleSubmit}>
       <Form.Input
         name="identifier"
         type="text"
         placeholder="E-mail"
+        onChange={formik.handleChange}
+        error={formik.errors.identifier}
       />
       <Form.Input
         name="password"
         type="password"
         placeholder="Senha"
+        onChange={formik.handleChange}
+        error={formik.errors.password}
       />
       <div className="actions">
         <Button
@@ -41,4 +54,18 @@ export default function LoginForm({ showRegisterForm }) {
       </div>
     </Form>
   )
+}
+
+function initialValues() {
+  return {
+    identifier: '',
+    password: ''
+  }
+}
+
+function validationSchema() {
+  return {
+    identifier: Yup.string().email(true).required('Campo obrigátorio!'),
+    password: Yup.string().required('Campo obrigátorio!')
+  }
 }
