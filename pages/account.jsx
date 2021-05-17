f8cca58ac4f8957fa82ff940c32684ad6e970ef3
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-17 09:08:28 -0300

    feat: Blocking unauthenticated users

--- pages/account.jsx
+++ pages/account.jsx
@@ -1,9 +1,43 @@
+import { useState, useEffect } from 'react' 
+import { useRouter } from 'next/router'
+
+import useAuth from '../hooks/useAuth'
+
+import { getMeApi } from '../api/user'
+
 import BasicLayout from '../layouts/BasicLayout'
 
 export default function Account() {
+  const [user, setUser] = useState(undefined)
+  const { auth, logout } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    (async () => {
+      const response = await getMeApi(logout)
+      setUser(response || null)
+    })()
+  }, [auth])
+
+  if (user === undefined) return null
+
+  if (!auth && !user) {
+    router.replace('/')
+    return null
+  }
+
   return (
     <BasicLayout className="account">
-      <h1>Account</h1>
+      <Configuration />
     </BasicLayout>
+  )
+}
+
+function Configuration() {
+  return (
+    <div className="account__configuration">
+      <div className="title">Configuração</div> 
+      <div className="data">Formulário</div> 
+    </div>
   )
 }
