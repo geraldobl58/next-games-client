23c373d66380df5bf1b679ca158cc28a5be26355
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-18 09:46:35 -0300

    feat: Rendering all games

--- pages/games/[platform].jsx
+++ pages/games/[platform].jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
+import { Loader } from 'semantic-ui-react'
+
+import { size } from 'lodash'
+
 import BasicLayout from '../../layouts/BasicLayout'
+import ListGames from '../../components/ListGames'
 
 import { getGamesPlatformApi } from '../../api/game'
 
-const limitPerPage = 1
+const limitPerPage = 2
 
 export default function Platform() {
   const [games, setGames] = useState(null)
@@ -13,14 +18,22 @@ export default function Platform() {
 
   useEffect(() => {
     (async () => {
-      const response = await getGamesPlatformApi(query.platform, limitPerPage, 0)
-      setGames(response)
+      if (query.platform) {
+        const response = await getGamesPlatformApi(query.platform, limitPerPage, 0)
+        setGames(response)
+      }
     })()
   }, [query])
 
   return (
     <BasicLayout className="platform">
-      <h1>{query.platform}</h1>
+      {!games && <Loader active />}
+      {games && size(games) === 0 && (
+        <div>
+          <h3>Não existe jogos no momento!</h3>
+        </div>
+      )}
+      {size(games) > 0 && <ListGames games={games} />}
     </BasicLayout>
   )
 }
