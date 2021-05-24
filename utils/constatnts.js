5ed921f69857791a6982ff872393f73eae8de9c8
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-24 15:40:36 -0300

    chore: Changing URL to communicate with API

--- utils/constatnts.js
+++ utils/constatnts.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:1337'
+export const BASE_URL = 'https://next-games-api.herokuapp.com'
 export const TOKEN = 'token'
 export const CART = 'cart'
 export const STRIPE_TOKEN = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
